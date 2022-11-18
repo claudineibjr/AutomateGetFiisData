a243d0e3c2a8bdc7a9197b9146345798b95ba1a6
Claudinei B Jr <claudineibjr@hotmail.com>
2022-11-18 17:00:41 -0300

    Documentation on FIIData

--- model/FIIData.py
+++ model/FIIData.py
@@ -2,15 +2,20 @@ from typing import NamedTuple
 from .HistoryData import HistoryData
 
 class FIIData(NamedTuple):
-  ticker: str
-  name: str
-  sector: str
-  segment: str
-  price: str
-  assetValue: str
-  incomeValue: str
-  liquidity: str
-  vacancy: str
-  grossLeasableArea: str
-  patrimony: str
+  ticker: str # Papel
+  name: str # Descrição
+  sector: str # Setor
+  segment: str # Segmento
+  price: str # Valor Cota
+  assetValue: str # Valor Patrimonial
+  incomeValue: str # Dividendo
+  liquidity: str # Liquidez
+  vacancy: str # Vacância
+  grossLeasableArea: str # Área bruta locável (m²)
+  patrimony: str # Patrimônio
   historicalDataList: list[HistoryData]
+
+ # Nº Ativos
+ # Nº Contratos
+ # Score FE
+ # Risco
