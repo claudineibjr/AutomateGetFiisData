2a5d65cf84959c4bec10f770665d4e13f4be6386
Claudinei B Jr <claudineibjr@hotmail.com>
2022-11-22 22:04:57 -0300

    Implementing readFromGoogleSheet

--- googleSheet/readFromGoogleSheet.py
+++ googleSheet/readFromGoogleSheet.py
@@ -3,24 +3,24 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import pandas as pd
 
-from env import SPREADSHEET_ID, SPREADSHEET_RANGE
+from env import SPREADSHEET_ID, SPREADSHEET_READ_RANGE
 
-def readFromGoogleSheet(creds: Credentials):
+def readFromGoogleSheet(creds: Credentials) -> pd.DataFrame:
   try:
     # Call the Sheets API
     service = build('sheets', 'v4', credentials=creds)
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=SPREADSHEET_RANGE).execute()
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=SPREADSHEET_READ_RANGE).execute()
     values = result.get('values', [])
 
     # Create a data frame
     data = pd.DataFrame(values)
-    data.columns = data.iloc[0]
+
+    # Set the first row as header
+    new_header = data.iloc[0]
     data = data[1:]
+    data.columns = new_header
 
-    print(data)
-
-    if pd.DataFrame(data['Papel'] == 'KNRI11').bool():
-      print('Exist')
+    return data
   except HttpError as err:
     print(err)
