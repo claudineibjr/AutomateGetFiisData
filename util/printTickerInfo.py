0faaaa69e3a49bdd130f56e71967b58c10e3705f
Claudinei B Jr <claudineibjr@hotmail.com>
2022-11-23 08:35:29 -0300

    Fixing way to display vacancy

--- util/printTickerInfo.py
+++ util/printTickerInfo.py
@@ -2,6 +2,7 @@ from model.FIIData import FIIData
 
 from util.formatAsCurrency import formatAsCurrency
 from util.formatAsNumber import formatAsNumber
+from util.formatAsPercentage import formatAsPercentage
 
 def printTickerInfo(data: FIIData):
   print ('Nome: ' + data.name)
@@ -11,7 +12,7 @@ def printTickerInfo(data: FIIData):
   print ('Valor patrimonial: ' + formatAsCurrency(data.assetValue))
   print ('Dividendo: ' + formatAsCurrency(data.incomeValue))
   print ('Liquidez diária: ' + formatAsNumber(data.liquidity))
-  print ("Vacância: " + formatAsNumber(data.vacancy) * 100)
+  print ("Vacância: " + formatAsPercentage(data.vacancy))
   print ("Área bruta locável (m²): " + formatAsNumber(data.grossLeasableArea))
   print ("Patrimônio: " + formatAsCurrency(data.patrimony))
   print ("Número de ativos " + data.numberOfAssets)
